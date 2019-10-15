864cf006b5a12e308254f3cc614227d72a1ccf2a
andriputra <agus.ap.info@gmail.com>
2019-10-15 08:24:05 +0700

    ini adalah file pertama saya

---
+++ inipunyaagus.java
@@ -0,0 +1,15 @@
+public class segitiga3 {
+	public static void main (String[] args) {
+		int a, b;
+		for (a=1; a<=10; a++) //baris
+		{
+			for (b=a; b<=10; b++) //kolom
+			{
+			System.out.print (" "+b);
+			}
+		System.out.println ();
+		}	
+	}
+}
+
+
