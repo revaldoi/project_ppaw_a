8327c818b9c7e0a57bc9905e22faac84de8b50b1
andriputra <agus.ap.info@gmail.com>
2019-10-22 08:02:28 +0700

    saya hanya merubah text

--- inipunyaagus.java
+++ inipunyaagus.java
@@ -1,15 +1,1 @@
-public class segitiga3 {
-	public static void main (String[] args) {
-		int a, b;
-		for (a=1; a<=10; a++) //baris
-		{
-			for (b=a; b<=10; b++) //kolom
-			{
-			System.out.print (" "+b);
-			}
-		System.out.println ();
-		}	
-	}
-}
-
-
+saya tulis baru file nya biar keren
